77570539e5c9217683e783a103a4fde7ce3b40de
Aristona <iamaristona@windowslive.com>
2015-02-25 13:11:52 +0200

    Translated pagination and password files

--- tr/passwords.php
+++ tr/passwords.php
@@ -13,10 +13,10 @@ return [
 	|
 	*/
 
-	"password" => "Passwords must be at least six characters and match the confirmation.",
-	"user" => "We can't find a user with that e-mail address.",
-	"token" => "This password reset token is invalid.",
-	"sent" => "We have e-mailed your password reset link!",
-	"reset" => "Your password has been reset!",
+	"password" => "Şifreler altı karakter olmalı ve onay ile eşleşmelidir.",
+	"user" => "Bu e-mail adresi ile kayıtlı bir üye bulunmuyor.",
+	"token" => "Şifre sıfırlama bileti geçersiz.",
+	"sent" => "Şifre sıfırlama bileti başarı ile gönderildi!",
+	"reset" => "Şifreniz başarıyla sıfırlandı!",
 
 ];
