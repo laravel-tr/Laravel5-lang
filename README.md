2e25d563f2989513c16f2687039b2457f064aa6e
angelside <angelside@users.sourceforge.net>
2015-07-03 02:05:47 +0300

    Laravel 5.1.4 Yeni gelen "string" satırı eklendi. PSR-2 uyumluluğu, girintiler boşluk ile değiştirildi. Çift tırnaklar tek tırnak ile değiştirildi. README.md birkaç satır karalandı.

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# Laravel5-lang
-Laravel 5.* Türkçe dil dosyaları
+## Laravel 5.1.x Türkçe dil dosyaları
+
+> "[Laravel Türkiye](http://laravel.gen.tr/)" tarafından Laravel 5.1.x sürümleri için tercüme edilen ve geliştirilen Laravel Türkçe dil dosyalarıdır.
+
+
+### Dil dosyası kurulumu
+
+#### Dosyaların kopyalanması
+*tr/* dizinini */resources/lang/* dizini içerisine kopyalayınız, sonuç olarak */resources/lang/tr/* şeklinde olacaktır.
+
+#### Laravel yazılımını Türkçe dilde kullanma
+*config/app.php* dosyasındaki, **locale** kısmındaki **en** ifadesini **tr** olarak değiştiriniz. Sonuç aşağıdaki gibi olacaktır:
+
+```php
+//'locale' => 'en', 
+'locale' => 'tr', 
+```
+
+### Lisans
+Açık kaynaklı olan bu proje [MIT lisansı][mit-url] ile lisanslanmıştır.
+
+[mit-url]: http://opensource.org/licenses/MIT
--- tr/passwords.php
+++ tr/passwords.php
@@ -2,21 +2,21 @@
 
 return [
 
-	/*
-	|--------------------------------------------------------------------------
-	| Password Reminder Language Lines
-	|--------------------------------------------------------------------------
-	|
-	| The following language lines are the default lines which match reasons
-	| that are given by the password broker for a password update attempt
-	| has failed, such as for an invalid token or invalid new password.
-	|
-	*/
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reminder Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
 
-	"password" => "Parolalar en az altı karakter olmalı ve onay ile eşleşmelidir.",
-	"user" => "Bu e-posta adresi ile kayıtlı bir üye bulunmuyor.",
-	"token" => "Parola sıfırlama bileti geçersiz.",
-	"sent" => "Parola sıfırlama bileti e-posta ile gönderildi!",
-	"reset" => "Parolanız başarıyla sıfırlandı!",
+    'password'  => 'Parolalar en az altı karakter olmalı ve onay ile eşleşmelidir.',
+    'user'      => 'Bu e-posta adresi ile kayıtlı bir üye bulunmuyor.',
+    'token'     => 'Parola sıfırlama bileti geçersiz.',
+    'sent'      => 'Parola sıfırlama bileti e-posta ile gönderildi!',
+    'reset'     => 'Parolanız başarıyla sıfırlandı!',
 
 ];
