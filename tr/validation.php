5554904e45df389f9b4ebf32ed6e193e6061eac8
juysoft <package@juysoft.com>
2016-08-12 23:42:27 +0300

    Laravel 5.1.33 güncellemeleri

--- tr/validation.php
+++ tr/validation.php
@@ -36,8 +36,8 @@ return [
     'digits_between' => ':attribute :min ile :max arasında rakam olmalıdır.',
     'distinct'       => ':attribute tekrarlanan bir değere sahiptir.',
     'email'          => ':attribute doğru bir e-posta olmalıdır.',
-    'filled'         => 'Seçili :attribute alanı doldurulmak zorundadır.',
     'exists'         => 'Seçili :attribute geçersiz.',
+    'filled'         => 'Seçili :attribute alanı doldurulmak zorundadır.',
     'image'          => ':attribute alanı resim dosyası olmalıdır.',
     'in'             => ':attribute değeri geçersiz.',
     'integer'        => ':attribute rakam olmalıdır.',
@@ -84,7 +84,7 @@ return [
     |--------------------------------------------------------------------------
     |
     | Here you may specify custom validation messages for attributes using the
-    | convention 'attribute.rule' to name the lines. This makes it quick to
+    | convention "attribute.rule" to name the lines. This makes it quick to
     | specify a specific custom language line for a given attribute rule.
     |
     */
@@ -102,7 +102,7 @@ return [
     |
     | The following language lines are used to swap attribute place-holders
     | with something more reader friendly such as E-Mail Address instead
-    | of 'email'. This simply helps us make messages a little cleaner.
+    | of "email". This simply helps us make messages a little cleaner.
     |
     */
 
