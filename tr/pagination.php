5cab5d2ae427a38f6f8b2af49ae4c0a48d3a3c35
Sinan Eldem <sineld@gmail.com>
2018-03-20 12:50:33 +0300

    Update pagination.php

--- tr/pagination.php
+++ tr/pagination.php
@@ -1,7 +1,6 @@
 <?php
 
 return [
-
     /*
     |--------------------------------------------------------------------------
     | Pagination Language Lines
@@ -13,7 +12,6 @@ return [
     |
     */
 
-    'previous' => '&laquo; Geri',
-    'next'     => 'İleri &raquo;',
-
+    'previous' => '&laquo; Önceki',
+    'next' => 'Sonraki &raquo;',
 ];
